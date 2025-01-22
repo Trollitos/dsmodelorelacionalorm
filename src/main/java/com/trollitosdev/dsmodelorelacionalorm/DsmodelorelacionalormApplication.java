f6b35972d8c2c91c77c1dfe71541d287ee0dcc02
Vinicius Neri <vinicius.neri@outlook.com>
2025-01-22 16:27:52 -0300

    Projeto devsuperior modelo relacional e ORM

---
+++ src/main/java/com/trollitosdev/dsmodelorelacionalorm/DsmodelorelacionalormApplication.java
@@ -0,0 +1,13 @@
+package com.trollitosdev.dsmodelorelacionalorm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmodelorelacionalormApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmodelorelacionalormApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/trollitosdev/dsmodelorelacionalorm/entities/Participante.java
@@ -0,0 +1,66 @@
+package com.trollitosdev.dsmodelorelacionalorm.entities;
+
+import jakarta.persistence.*;
+import jakarta.servlet.http.Part;
+
+import java.util.*;
+
+@Entity
+@Table(name = "tb_participante")
+public class Participante {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String email;
+
+    @ManyToMany(mappedBy = "participantes")
+    private Set<Atividade> atividades = new HashSet<>();
+
+    public Participante() {
+    }
+
+    public Participante(Long id, String nome, String email) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Participante that = (Participante) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
